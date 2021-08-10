34cd88ae91412e7477e0473fc2a20dc205ad407e
bahar hashemi <bahar.hashemisharif@gmail.com>
2021-08-10 17:47:59 +0430

    create file

---
+++ components/sideBar/SideBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import {
+    color,
+    ColorProps,
+    display,
+    DisplayProps,
+    layout,
+    LayoutProps,
+    shadow,
+    ShadowProps,
+    space,
+    SpaceProps
+} from "styled-system";
+
